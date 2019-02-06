0a0601e692d2acd32ee498dd536e1558d81f67bc
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-06 16:12:18 -0500

    created the functions for building a deck and shuffling a deck. added those functions to the main function to be invoked when page is loaded

--- public/main.js
+++ public/main.js
@@ -1,7 +1,38 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let faces = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ]
+let suits = [ 'H', 'D', 'C', 'S' ]
+let unshuffledDeck = []
+let shuffledDeck = []
+
+const buildDeck = () => {
+  for (let i = 0; i < suits.length; i++) {
+    console.log(suits[i])
+
+    for (let j = 0; j < faces.length; j++) {
+      console.log(suits[i] + faces[j])
+      unshuffledDeck.push(suits[i] + faces[j])
+    }
   }
+  console.log(unshuffledDeck)
+}
+
+const main = () => {
+  // call buildDeck function
+  buildDeck()
+  // call shuffleDeck function
+  shuffleDeck()
+  // add Event Listener to deal a card on button click
+}
+
+const shuffleDeck = () => {
+  // for i from 53 - 1 down to 1 do:
+  // j = random integer (where 0 <= j <= i)
+  // swap items[i] with items[j]
+  for (i = unshuffledDeck.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    shuffledDeck.push(unshuffledDeck[j])
+    unshuffledDeck.splice(j, 1)
+  }
+  console.log(shuffledDeck)
 }
 
 document.addEventListener('DOMContentLoaded', main)
