fd510efd7096871f1e568ae864676221b61a1657
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-02-06 16:34:19 -0500

    added button tag to html to deal a card when clicking on card
    wrote the fisher yates algorithm in order to shuffle the deck

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 let faces = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ]
 let suits = [ 'H', 'D', 'C', 'S' ]
-let unshuffledDeck = []
-let shuffledDeck = []
+let deck = []
 
 const buildDeck = () => {
   for (let i = 0; i < suits.length; i++) {
@@ -9,10 +8,34 @@ const buildDeck = () => {
 
     for (let j = 0; j < faces.length; j++) {
       console.log(suits[i] + faces[j])
-      unshuffledDeck.push(suits[i] + faces[j])
+      deck.push(suits[i] + faces[j])
     }
   }
-  console.log(unshuffledDeck)
+  console.log(deck)
+}
+
+const shuffleDeck = () => {
+  // for i from 53 - 1 down to 1 do:
+  // j = random integer (where 0 <= j <= i)
+  // swap items[i] with items[j]
+  for (let i = deck.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+
+    let cardAtPositionI = deck[i]
+    let cardAtPositionJ = deck[j]
+
+    // swap items[i] with items[j]
+    deck[j] = cardAtPositionI
+    deck[i] = cardAtPositionJ
+
+    // shuffledDeck.push(unshuffledDeck[j])
+    // unshuffledDeck.splice(j, 1)
+  }
+  console.log(deck)
+}
+
+const dealCard = () => {
+  // click on card to display a single value of shuffleDeck array
 }
 
 const main = () => {
@@ -20,19 +43,9 @@ const main = () => {
   buildDeck()
   // call shuffleDeck function
   shuffleDeck()
-  // add Event Listener to deal a card on button click
-}
-
-const shuffleDeck = () => {
-  // for i from 53 - 1 down to 1 do:
-  // j = random integer (where 0 <= j <= i)
-  // swap items[i] with items[j]
-  for (i = unshuffledDeck.length - 1; i >= 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1))
-    shuffledDeck.push(unshuffledDeck[j])
-    unshuffledDeck.splice(j, 1)
-  }
-  console.log(shuffledDeck)
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+// add Event Listener to deal a card on button click
+document.querySelector('.deal-card').addEventListener('click', dealCard)
